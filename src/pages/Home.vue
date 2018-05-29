25e094f7d0bf8b5d74c45b6c06cfea77704d4b04
zhaoqi <2022145499@qq.com>
2018-05-29 11:09:29 +0800

    初始化

---
+++ src/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="header"></div>
+</template>
+
+<script>
+	export default{
+		name:'Home'
+	}
+</script>
+
+<style scoped="">
+.header{
+	width: 100%;
+	height: 45px;
+	background: #f00;
+}
+</style>
